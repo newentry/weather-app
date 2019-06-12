c40f1a5c6f472c8dc403adc520f1d7f04ad2cebd
newentry <undeiiandrei@yahoo.com>
2019-06-12 10:18:58 +0100

    weather-app

---
+++ main.js
@@ -0,0 +1,84 @@
+function show_hide(argument) {
+	var click = document.getElementById('dropdown-menu');
+	if (click.style.display === "none") {
+		click.style.display = "block";
+	}
+	else {
+		click.style.display = "none";
+	}
+}
+
+function getFocus() {
+	$("a").click(function(event){
+  	event.preventDefault();
+});
+	var focus2 = document.getElementById("location-search-input").focus();
+	return focus2;
+
+}
+
+function add(dtime, icon, weather, temp) {
+
+		var table ='<table border=1>\
+			<tr>\
+				<td class="date-time">' + dtime + '</td>\
+			</tr>\
+			<tr>\
+				<td class="current-forecast"><td>\
+			</tr>\
+			<tr>\
+				<td><img class="icon"></td>\
+			</tr>\
+			<tr>\
+				<td class="temp">' + temp + '</td>\
+			</tr>\
+			<tr>\
+				<td class="weather">' + weather + '</td>\
+			</tr>\
+		</table>';
+		
+		
+		$("#content-weather").append(table);
+
+		/*$(".date-time").append(dtime);   
+		$(".icon").attr("src", icon);
+		$(".weather").append(weather);
+		$(".temp").append(temp); 
+		console.log(dtime);*/
+}
+
+function xlist(list, index) {
+		
+		
+		var dtime = list[index].dt_txt;
+		var icon = "http://openweathermap.org/img/w/" + list[index].weather[0].icon + ".png";
+		var weather = list[index].weather[0].main;
+		var temp = Math.floor(list[index].main.temp);
+		
+		
+		
+	add(dtime, icon, weather, temp);
+}
+
+function location2(event) {
+	var city = document.getElementById("location-search-input").value;
+	
+$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=metric&appid=5b5393bafaa87fba80fc2c0ab113981e", 
+	
+	function(data){
+		console.log(data);
+		
+		var day = "Today"
+		$("#content-weather").html(day);
+		var i;
+		
+		for( i=0; i<data.list.length; i++){
+			
+			xlist(data.list, i);
+			console.log(xlist);
+		}
+	}
+	);
+	
+	return false;
+}
